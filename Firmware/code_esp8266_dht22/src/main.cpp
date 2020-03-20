b80ff72edc575c6529187caaff4c57449264d8e2
BrunoRicci <bruno.ricci.963@gmail.com>
2020-03-20 20:07:09 -0300

    One DHT object per each sensor, working fine (only random bad contact in breadboard pins). Reset citcuit corrected and data gathering fine..

--- Firmware/code_esp8266_dht22/src/main.cpp
+++ Firmware/code_esp8266_dht22/src/main.cpp
@@ -10,18 +10,19 @@
 #include <Adafruit_Sensor.h>
 
 #include <DHT.h>
-#define DHT_SENSOR_1 5
-#define DHT_SENSOR_2 4
-#define DHT_SENSOR_3 0
-#define DHT_SENSOR_4 13
+#define DHT_SENSOR_1_PIN 2
+#define DHT_SENSOR_2_PIN 0
+#define DHT_SENSOR_3_PIN 4
+#define DHT_SENSOR_4_PIN 5
 
-
-//pinMode(_pin, INPUT_PULLUP)
 #define DHTTYPE DHT22
-DHT dht22_sensor_1(DHT_SENSOR_1, DHTTYPE);    //Creates DHT22 sensor "sensor" object
-//DHT dht22_sensor_2(DHTPIN_2, DHTTYPE);    //Creates DHT22 sensor "sensor" object
+DHT dht22_sensor_1(DHT_SENSOR_1_PIN, DHTTYPE);    //Creates DHT22 sensor "sensor" object
+DHT dht22_sensor_2(DHT_SENSOR_2_PIN, DHTTYPE);    //Creates DHT22 sensor "sensor" object
+DHT dht22_sensor_3(DHT_SENSOR_3_PIN, DHTTYPE);    //Creates DHT22 sensor "sensor" object
+DHT dht22_sensor_4(DHT_SENSOR_4_PIN, DHTTYPE);    //Creates DHT22 sensor "sensor" object
 
-#define PWR_SENSORS  14
+
+#define PWR_SENSORS_PIN  13
 #define ON  1
 #define OFF 0
 
@@ -39,23 +40,24 @@ ESP8266WiFiMulti WiFiMulti;
 
 void setup() {
   dht22_sensor_1.begin();           //Initializes object.
-  // dht22_sensor_2.begin();           //Initializes object.
-
-  portInit();
+  dht22_sensor_2.begin();
+  dht22_sensor_3.begin();
+  dht22_sensor_4.begin();
   
+  portInit();
 
   Serial.begin(115200);
   // Serial.setDebugOutput(true);
 
-  WiFi.mode(WIFI_STA);
-  WiFiMulti.addAP("Fibertel WiFi866 2.4GHz", "01416592736");
+  // WiFi.mode(WIFI_STA);
+  // WiFiMulti.addAP("Fibertel WiFi866 2.4GHz", "01416592736");
 
 }
 
 void loop() {
 
   //waits for WiFi connection
-  if ((WiFiMulti.run() == WL_CONNECTED)) {
+  /* if ((WiFiMulti.run() == WL_CONNECTED)) {
     Serial.printf("WiFi Connected!");
     
     WiFiClient client;
@@ -90,59 +92,26 @@ void loop() {
     else {
       Serial.printf("[HTTP] Unable to connect\n");
     }
-  }
+  } */
 
-  digitalWrite(PWR_SENSORS, HIGH);
-  dht22_sensor_1.begin();
+  Serial.printf("Turning on sensor...");
+  SetSensorPower(ON);
+  // dht22_sensor_1.begin();           //Initializes object.
   delay(800);
-  dht22_sensor_1.setPin(DHT_SENSOR_1);
-  temperature = dht22_sensor_1.readTemperature(false, true);   //Won't force conversion as the first measurement.
-  humidity = dht22_sensor_1.readHumidity(false);
-  Serial.printf("\nTemperature_1: %f\n", temperature );
-  Serial.printf("Humidity_1: %f\n", humidity );
 
-  dht22_sensor_1.setPin(DHT_SENSOR_2);
-  temperature = dht22_sensor_1.readTemperature(false, true);   //Forces conversion as it's a different sensor than the last one.
-  humidity = dht22_sensor_1.readHumidity(false);
-  Serial.printf("Temperature_2: %f\n", temperature );
-  Serial.printf("Humidity_2: %f\n", humidity );
+  Serial.printf("\nTemperature_1: %f\n", dht22_sensor_1.readTemperature() );
+  Serial.printf("Humidity_1: %f\n", dht22_sensor_1.readHumidity() );
 
-  dht22_sensor_1.setPin(DHT_SENSOR_3);
-  temperature = dht22_sensor_1.readTemperature(false, true);   //Forces conversion as it's a different sensor than the last one.
-  humidity = dht22_sensor_1.readHumidity(false);
-  Serial.printf("Temperature_3: %f\n", temperature );
-  Serial.printf("Humidity_3: %f\n", humidity );
+  Serial.printf("\nTemperature_2: %f\n", dht22_sensor_2.readTemperature() );
+  Serial.printf("Humidity_2: %f\n", dht22_sensor_2.readHumidity() );
 
-  dht22_sensor_1.setPin(DHT_SENSOR_4);
-  temperature = dht22_sensor_1.readTemperature(false, true);   //Forces conversion as it's a different sensor than the last one.
-  humidity = dht22_sensor_1.readHumidity(false);
-  Serial.printf("Temperature_4: %f\n", temperature );
-  Serial.printf("Humidity_4: %f\n", humidity );
+  Serial.printf("\nTemperature_3: %f\n", dht22_sensor_3.readTemperature() );
+  Serial.printf("Humidity_3: %f\n", dht22_sensor_3.readHumidity() );
 
-  digitalWrite(PWR_SENSORS, LOW);
+  Serial.printf("\nTemperature_4: %f\n", dht22_sensor_4.readTemperature() );
+  Serial.printf("Humidity_4: %f\n", dht22_sensor_4.readHumidity() );
   
-  /* digitalWrite(PWR_SENSOR_2, HIGH);
-  temperature = dht22_sensor_1.readTemperature();
-  humidity = dht22_sensor_1.readHumidity();
-  Serial.printf("\nTemperature_1: %f\n", temperature );
-  Serial.printf("Humidity_1: %f\n", humidity );
-  digitalWrite(PWR_SENSOR_2, LOW); */
-
-
-  
-  /* temperature = dht22_sensor_1.readTemperature();
-  humidity = dht22_sensor_1.readHumidity();
-  Serial.printf("\nTemperature_1: %f\n", temperature );
-  Serial.printf("Humidity_1: %f\n", humidity ); */
-
-  /* temperature = dht22_sensor_2.readTemperature();
-  humidity = dht22_sensor_2.readHumidity();
-  Serial.printf("Temperature_2: %f \n", temperature );
-  Serial.printf("Humidity_2: %f\n", humidity ); */
-
-  Serial.printf("millis(): %d\n", millis());
-  Serial.printf("micros(): %d\n", micros());
-
+  // SetSensorPower(OFF);
 
   Serial.printf("Going deep sleep...  rtc_time=%d", system_get_rtc_time() );
   ESP.deepSleep(5e6);  //deep sleeps for 5 seconds...
@@ -155,15 +124,15 @@ void loop() {
 void SetSensorPower(unsigned char state){
   if (state == ON)
   {
-    digitalWrite(PWR_SENSORS, HIGH);  //Turn sensors supply on.
+    digitalWrite(PWR_SENSORS_PIN, LOW);  //Turn sensors supply on.
   }
   if (state == OFF)
   {
-    digitalWrite(PWR_SENSORS, LOW);  //Turn sensors supply off.
+    digitalWrite(PWR_SENSORS_PIN, HIGH);  //Turn sensors supply off.
   }
 }
 
 void portInit(void){
-  pinMode(PWR_SENSORS, OUTPUT);   //GPIO12 as output
+  pinMode(PWR_SENSORS_PIN, OUTPUT);   //GPIO12 as output
   SetSensorPower(OFF);          //Sensors start turned off.
 }
